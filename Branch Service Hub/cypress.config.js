9f9a7fe68aca6a03e21c92fae2c18afa6feebf8d
Felix <bosirefelix97@gmail.com>
2023-07-24 22:21:20 +0300

    add

---
+++ Branch Service Hub/cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+
+
+module.exports = defineConfig({
+  e2e: {
+    watchForFileChanges: false,
+    chromeWebSecurity:false,
+    experimentalModifyObstructiveThirdPartyCode: true,
+    experimentalSessionAndOrigin:true,
+    experimentalStudio:true,
+    experimentalModifyObstructiveThirdPartyCode:true,
+    retries: {
+      "runMode": 0,
+      "openMode": 0
+     },
+    setupNodeEvents(on, config) {
+  
+    },
+    
+  },
+});
